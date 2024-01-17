1288b9e4fdda6db66d69265ab598fb1e61653350
Hunter Harrison <hunterharrisondev@gmail.com>
2024-01-17 12:26:38 -0600

    add banner

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 
-  const navItems = document.getElementsByClassName('nav-item-list');
+  // variable declarations
+  const navItems = document.getElementsByClassName('nav-item');
 
+  // function declarations
   function addActiveClassToNavItem(event) {
-    event.preventDefault();
     const navItemId = event.target.id;
-    const navItem = document.getElementById(navItemId);
     const classToBeAdded = 'active';
-    if (navItem.classList.contains(classToBeAdded)) {
-      navItem.classList.remove(classToBeAdded);
-    } else {
-      navItem.classList.add(classToBeAdded);
+
+    for(let i = 0; i < navItems.length; i++) {
+      const navItemContainsClass = navItems[i].classList.contains(classToBeAdded);
+      if (navItems[i].id === navItemId && !navItemContainsClass) {
+        navItems[i].classList.add(classToBeAdded)
+      } else if (navItems[i].id !== navItemId && navItemContainsClass) {
+        navItems[i].classList.remove(classToBeAdded)
+      }
     }
   }
 
+
+  function findActiveLink() {
+    const activeURL = window.location.href.split('#')[1];
+    const idToFind = `nav-item-${activeURL}`;
+    document.getElementById(idToFind).classList.add('active');
+  }
+
+  //init
+  findActiveLink();
+  //event listners
   for(let i = 0; i < navItems.length; i++) {
     navItems[i].addEventListener('click', addActiveClassToNavItem);
   }
